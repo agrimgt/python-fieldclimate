c27240a87ff6584ae040549a26c26fddd7a02fb4
Phillip Marshall <pmarshall@agrimgt.com>
2019-09-17 23:44:22 -0700

    fixed django tests: settings must be configured here

--- tests/test_django.py
+++ tests/test_django.py
@@ -5,13 +5,10 @@ from django.conf import settings
 from fieldclimate.django import DjangoFieldClimateClient
 from tests.utils import async_test
 
+settings.configure(FIELDCLIMATE_PUBLIC_KEY="DJANGO", FIELDCLIMATE_PRIVATE_KEY="DANGO")
+
 
 class DjangoTestCase(TestCase):
-    def setUp(self):
-        settings.configure(
-            FIELDCLIMATE_PUBLIC_KEY="DJANGO", FIELDCLIMATE_PRIVATE_KEY="DANGO"
-        )
-
     @async_test
     async def test_settings_keys(self):
         # Ensure that public and private keys are set according to django's settings:
